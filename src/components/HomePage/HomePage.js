ac6b52c11efa4e334946655fa3569cb14e3b3c5a
Rohan <rhnmht30@gmail.com>
2019-03-10 17:53:00 +0530

    set-up

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./HomePage.css";
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello homePage</h1>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
